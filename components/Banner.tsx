b24a96b937c4e8c75116cecd501fb940bcf137c1
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-11-16 15:07:28 -0800

    Create Banner.tsx

---
+++ components/Banner.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+type Props = {};
+
+const Banner = (props: Props) => {
+  return (
+    <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl-h-[600px] 2xl:h-[700px]">
+      <Image className="object-cover" src="/banner1200x600.jpg" fill alt="banner" />
+      <div className="absolute top-1/2 w-full text-center">
+        <p className="text-slate-100 text-sm sm:text-lg">Do you want to find our best hotels?</p>
+        <button className="text-red-600 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150">I'm feeling lucky</button>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
