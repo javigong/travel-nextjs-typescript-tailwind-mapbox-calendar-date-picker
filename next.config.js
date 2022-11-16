bdf88408226524ac24028b22e43e303510fcf82f
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-11-16 15:08:14 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["t1.gstatic.com", "t2.gstatic.com", "t3.gstatic.com", "upload.wikimedia.org"],
+  },
+};
