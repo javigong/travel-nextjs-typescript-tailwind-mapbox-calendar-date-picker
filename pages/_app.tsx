2c4684c230b45ecc1dc6c4a2201af3b3ccd4252b
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-11-16 20:47:25 -0800

    Add ProgressBar wrapping all the App

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import ProgressBar from "@badrap/bar-of-progress";
+import type { AppProps } from "next/app";
+import { Router } from "next/router";
+import "../styles/globals.css";
+
+const progress = new ProgressBar({
+  size: 4,
+  color: "orange",
+  className: "z-50",
+  delay: 80,
+});
+
+Router.events.on("routeChangeStart", progress.start);
+Router.events.on("routeChangeComplete", progress.finish);
+Router.events.on("routeChangeError", progress.finish);
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
