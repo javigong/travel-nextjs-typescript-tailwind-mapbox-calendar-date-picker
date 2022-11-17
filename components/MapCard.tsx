04256ccd14b97d94e4b91a0bdd7ad7368049f173
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-11-17 15:26:35 -0800

    Create MapCard with react-map-gl and geolib

---
+++ components/MapCard.tsx
@@ -0,0 +1,106 @@
+import { StarIcon } from "@heroicons/react/24/solid";
+import { getCenter } from "geolib";
+import "mapbox-gl/dist/mapbox-gl.css";
+import Image from "next/image";
+import { useState } from "react";
+import Map, { Marker, Popup } from "react-map-gl";
+import { IResult } from "../typings";
+
+type Props = {
+  searchResults: IResult[];
+};
+
+const MapCard = ({ searchResults }: Props) => {
+  const [selectedLocation, setSelectedLocation] = useState<IResult | null>(
+    null
+  );
+  const coordinates = searchResults.map((result) => ({
+    latitude: result.lat,
+    longitude: result.long,
+  }));
+  const center: any = getCenter(coordinates);
+  const [viewport, setViewport] = useState({
+    style: {
+      width: "100%",
+      height: "calc(100% - 68px)",
+    },
+    initialViewState: {
+      longitude: center.longitude,
+      latitude: center.latitude,
+      zoom: 11,
+    },
+  });
+
+  return (
+    <div className="sticky top-[68px] w-full h-screen">
+      <Map
+        {...viewport}
+        mapStyle="mapbox://styles/javiergongora/clalbftnj000g15nsx3nbjynw"
+        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
+      >
+        {searchResults.map((result) => (
+          <div key={result.long}>
+            <Marker
+              latitude={result.lat}
+              longitude={result.long}
+              offset={[-20, -10]}
+            >
+              <p
+                role="img"
+                onPointerOver={() => {
+                  setSelectedLocation(result);
+                }}
+                className="cursor-pointer text-2xl animate-bounce"
+                aria-label="push-pin"
+              >
+                📌
+              </p>
+            </Marker>
+
+            {/* popup should show if we click on a marker */}
+            {selectedLocation?.long === result.long ? (
+              <Popup
+                onClose={() => setSelectedLocation(null)}
+                closeOnClick={true}
+                latitude={result.lat}
+                longitude={result.long}
+              >
+                <div className="flex space-x-2">
+                  <div className="relative h-[100px] w-[150px]">
+                    <Image
+                      className="object-cover"
+                      src={result.img}
+                      alt={result.title}
+                      fill
+                    />
+                  </div>
+                  <div className="h-[100px] w-[180px]">
+                    <div>{result.title}</div>
+                    <div className="flex justify-between items-end pt-5">
+                      <p className="flex items-center">
+                        <StarIcon className="h-5 text-red-400" /> {result.star}
+                      </p>
+
+                      <div>
+                        <p className="text-xs lg:text-32xl font-semibold ">
+                          {result.price}
+                        </p>
+                        <p className="text-right font-extralight">
+                          {result.total}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </Popup>
+            ) : (
+              false
+            )}
+          </div>
+        ))}
+      </Map>
+    </div>
+  );
+};
+
+export default MapCard;
