9614133638239e714e3a1adfcbb11499a73c5967
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-11-16 20:47:46 -0800

    Add result interface

--- typings.d.ts
+++ typings.d.ts
@@ -8,3 +8,15 @@ export interface IStyleData {
   img: string;
   title: string;
 }
+
+export interface IResult {
+  description: string;
+  img: string;
+  lat: number;
+  location: string;
+  long: number;
+  price: string;
+  star: number;
+  title: string;
+  total: string;
+}
