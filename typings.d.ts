70b665d93bf7df6d6757ee21b9894e98b6c6addb
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-11-17 21:44:57 -0800

    Update typings.d.ts

--- typings.d.ts
+++ typings.d.ts
@@ -20,3 +20,27 @@ export interface IResult {
   title: string;
   total: string;
 }
+
+export interface IOptions {
+  method: string;
+  headers: {
+    "X-RapidAPI-Key": string;
+    "X-RapidAPI-Host": string;
+  };
+}
+
+export interface ISuggestion {
+  regionNames: {
+    shortName: string;
+    displayName: string;
+  };
+  gaiaId: number;
+  type: string,
+}
+
+export interface ISuggestionFormatted {
+  shortName: string,
+  displayName: string,
+  id: number,
+  type: string,
+}
