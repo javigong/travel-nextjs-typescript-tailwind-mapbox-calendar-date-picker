0e6491c0a5c3cb3f73a5f17deb6bdfc445ca7fef
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-11-17 21:45:04 -0800

    Create getCitySuggestions.ts

---
+++ utils/getCitySuggestions.ts
@@ -0,0 +1,28 @@
+import { IOptions, ISuggestion } from "../typings";
+
+const getCitySuggestions = async (word: string, setData: any) => {
+  const options: IOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPIDAPI_KEY!,
+      "X-RapidAPI-Host": "hotels4.p.rapidapi.com",
+    },
+  };
+  const data = await fetch(
+    `https://hotels4.p.rapidapi.com/locations/v3/search?q=${word}&locale=en_CA`,
+    options
+  );
+  const json = await data.json();
+  console.log({json});
+
+  const suggestionsFormatted = json.sr.map((suggestion: ISuggestion) => ({
+    shortName: suggestion.regionNames.shortName,
+    displayName: suggestion.regionNames.displayName,
+    id: suggestion.gaiaId,
+    type: suggestion.type,
+  }));
+
+  setData(suggestionsFormatted);
+};
+
+export default getCitySuggestions;
