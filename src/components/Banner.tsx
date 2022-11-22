138994e036d3e3dbbea0fcea8663549986e1ee4d
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-11-22 15:37:51 -0800

    Update Banner.tsx

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,14 +1,32 @@
+import { useSession } from "next-auth/react";
 import Image from "next/image";
 
 type Props = {};
 
 const Banner = (props: Props) => {
+  const { data: session, status } = useSession();
+
   return (
     <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl-h-[600px] 2xl:h-[700px]">
-      <Image className="object-cover" src="/banner1200x600.jpg" fill alt="banner" priority />
+      <Image
+        className="object-cover"
+        src="/banner1200x600.jpg"
+        fill
+        alt="banner"
+        priority
+      />
       <div className="absolute top-1/2 w-full text-center">
-        <p className="text-slate-100 text-sm sm:text-lg">Don't you know where to travel?</p>
-        <button className="text-red-600 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150">I'm feeling lucky</button>
+        {session && (
+          <p className="text-white text-base sm:text-2xl pb-2">
+            Hi, Javier Gongora!
+          </p>
+        )}
+        <p className="text-slate-100 text-sm sm:text-lg">
+          Don't you know where to travel?
+        </p>
+        <button className="text-red-600 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150">
+          I'm feeling lucky
+        </button>
       </div>
     </div>
   );
