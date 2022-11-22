4e527eac789c18b0bf2b4a0e1b2af1c3aafdde94
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-11-22 15:38:11 -0800

    Create favorites.tsx

---
+++ src/pages/favorites.tsx
@@ -0,0 +1,94 @@
+import { format } from "date-fns";
+import { GetServerSidePropsContext } from "next";
+import { signOut } from "next-auth/react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import Drawer from "../components/Drawer";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import InfoCard from "../components/InfoCard";
+import MapCard from "../components/MapCard";
+import { IResult } from "../types/typings";
+import getHotelList from "../utils/getHotelList";
+
+type Props = {
+  searchResults: IResult[];
+};
+
+const Favorites = ({ searchResults }: Props) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <div>
+      <Header isOpen={isOpen} setIsOpen={setIsOpen} />
+
+      <main className="flex">
+        {/* left section */}
+        <section className="flex-grow pt-14 px-6">
+          <p className="text-xs">
+            Accommodation list
+          </p>
+
+          <h1 className="text-3xl font-semibold mt-2 mb-6">
+            Your Favorites
+          </h1>
+
+          <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
+            <p className="button">Cancellation Flexibility</p>
+            <p className="button">Price</p>
+            <p className="button">Rooms and Beds</p>
+            <p className="button">More filters</p>
+          </div>
+          <div className="flex flex-col">
+            {/* map search results data */}
+            {/* {searchResults.map((item) => (
+              <InfoCard key={item.img} item={item} />
+            ))} */}
+          </div>
+        </section>
+
+        {/* right section with map */}
+        <section className="hidden lg:inline-flex flex-grow xl:min-w-[600px]">
+          {/* <MapCard searchResults={searchResults} /> */}
+        </section>
+      </main>
+      <Footer />
+
+      {/* Drawer */}
+      <Drawer isOpen={isOpen} setIsOpen={setIsOpen}>
+        <p className="drawer-current-item">
+          List of Favorites
+        </p>
+        <p className="drawer-item">
+        <Link href={"/bookings"}>Your Bookings</Link>
+          </p>
+        <p onClick={() => signOut()} className="drawer-item">
+          Sign out
+        </p>
+      </Drawer>
+    </div>
+  );
+};
+
+export default Favorites;
+
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  // const { id, location, startDate, endDate, numOfGuests } = context.query;
+  // TODO: use context to get the router query params to fetch from a third api
+  // const searchResults = await getHotelList(
+  //   id,
+  //   location,
+  //   startDate,
+  //   endDate,
+  //   numOfGuests
+  // ).catch(console.error);
+
+  return {
+    props: {
+      // searchResults,
+    },
+  };
+};
