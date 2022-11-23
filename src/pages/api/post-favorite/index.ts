82e80e8dc9f001c076e436343d8928d878e4efe7
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-11-23 10:30:50 -0800

    Add api endpoint to post favourite hotel

---
+++ src/pages/api/post-favorite/index.ts
@@ -0,0 +1,39 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/react";
+import prisma from "../../../server/db/prismadb";
+
+// POST /api/post
+// Required fields in body: title
+// Optional fields in body: content
+export default async function handle(req: NextApiRequest, res: NextApiResponse) {
+  const {
+    hotelId,
+    description,
+    img,
+    lat,
+    location,
+    long,
+    price,
+    star,
+    title,
+    total,
+  } = req.body;
+
+  const session = await getSession({ req });
+  const result = await prisma.hotel.create({
+    data: {
+      hotelId,
+      description,
+      img,
+      lat,
+      location,
+      long,
+      price,
+      star,
+      title,
+      total,
+      userEmail: session?.user?.email!,
+    },
+  });
+  res.json(result);
+}
