b6f16ccfe3850514d531e792a698b59407f93035
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-11-28 17:53:09 -0800

    Create new api endpoint to get all user bookings

---
+++ src/pages/api/get-bookings/index.ts
@@ -0,0 +1,20 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../../server/db/prismadb";
+
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { userEmail } = req.query;
+  if (req.method === "GET") {
+    const user = await prisma.user.findUnique({
+      where: { email: userEmail as string },
+      select: { bookings: true },
+    });
+    res.json(user);
+  } else {
+    throw new Error(
+      `The HTTP ${req.method} method is not supported at this route.`
+    );
+  }
+}
