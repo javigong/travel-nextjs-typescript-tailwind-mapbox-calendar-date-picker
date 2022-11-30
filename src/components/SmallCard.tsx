3eccede7e48aac624c44d7783da8d916b33036e7
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-11-30 15:42:41 -0800

    Add results route to each Canadian city with default dates and num of guests

--- src/components/SmallCard.tsx
+++ src/components/SmallCard.tsx
@@ -1,4 +1,6 @@
+import { addDays } from "date-fns";
 import Image from "next/image";
+import { useRouter } from "next/router";
 import { ICityData } from "../types/typings";
 
 type Props = {
@@ -6,9 +8,26 @@ type Props = {
 };
 
 const SmallCard = ({ cityData }: Props) => {
+  const router = useRouter();
+  const startDate = addDays(new Date(), 4);
+  const endDate = addDays(new Date(), 7);
+  const numOfGuests = 1;
+  const search = () => {
+    router.push({
+      pathname: "/search",
+      query: {
+        location: cityData.location,
+        id: cityData.id,
+        startDate: startDate.toISOString(),
+        endDate: endDate.toISOString(),
+        numOfGuests,
+      },
+    });
+  };
+
   return (
-    <div className="flex items-center m-2 space-x-4 rounded-xl cursor-pointer hover:bg-gray-100 hover:scale-105 transition transform duration-200 ease-out">
-      {/* left side */}
+    <div onClick={search} className="flex items-center m-2 space-x-4 rounded-xl cursor-pointer hover:bg-gray-100 hover:scale-105 transition transform duration-200 ease-out">
+      {/* Left side */}
       <div className="relative h-16 w-16">
         <Image
           className="rounded-lg"
@@ -17,7 +36,7 @@ const SmallCard = ({ cityData }: Props) => {
           fill
         />
       </div>
-      {/* right side */}
+      {/* Right side */}
       <div>
         <h2>{cityData.location}</h2>
         <h3 className="text-gray-500">{cityData.province}</h3>
