b3336face4ca2ecfc4c10f9d14dced04b5b0a9b8
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-11-30 15:40:17 -0800

    Update Banner.tsx

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,10 +1,35 @@
+import { addDays } from "date-fns";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
+import { useRouter } from "next/router";
+import { IInspiredCity } from "../types/typings";
 
-type Props = {};
+type Props = {
+  getInspiredCities: IInspiredCity[];
+};
 
-const Banner = (props: Props) => {
+const Banner = ({ getInspiredCities }: Props) => {
   const { data: session } = useSession();
+  const router = useRouter();
+  const startDate = addDays(new Date(), 4);
+  const endDate = addDays(new Date(), 7);
+  const numOfGuests = 1;
+  
+  const luckyCity =
+    getInspiredCities[Math.floor(Math.random() * getInspiredCities.length)];
+
+  const search = () => {
+    router.push({
+      pathname: "/search",
+      query: {
+        location: luckyCity.location,
+        id: luckyCity.id,
+        startDate: startDate.toISOString(),
+        endDate: endDate.toISOString(),
+        numOfGuests,
+      },
+    });
+  };
 
   return (
     <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl-h-[600px] 2xl:h-[700px]">
@@ -24,7 +49,10 @@ const Banner = (props: Props) => {
         <p className="text-slate-100 text-sm sm:text-lg">
           Don't you know where to travel?
         </p>
-        <button className="text-red-600 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150">
+        <button
+          onClick={search}
+          className="text-red-600 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150"
+        >
           I'm feeling lucky
         </button>
       </div>
