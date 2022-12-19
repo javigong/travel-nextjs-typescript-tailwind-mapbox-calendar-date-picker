a3a7cd2db7bd1904dce0913e7146732e83f303f6
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-12-19 15:52:37 -0800

    Update SmallCard.tsx

--- src/components/SmallCard.tsx
+++ src/components/SmallCard.tsx
@@ -1,45 +1,52 @@
 import { addDays } from "date-fns";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { ICityData } from "../types/typings";
+import { Dispatch, SetStateAction } from "react";
+import { ISuggestionFormatted } from "../types/typings";
 
 type Props = {
-  cityData: ICityData;
+  cityData: ISuggestionFormatted;
+  setSearchInput: Dispatch<SetStateAction<string>>;
+  setSelectedCity: Dispatch<SetStateAction<ISuggestionFormatted | null>>;
 };
 
-const SmallCard = ({ cityData }: Props) => {
+const SmallCard = ({ cityData, setSearchInput, setSelectedCity }: Props) => {
   const router = useRouter();
   const startDate = addDays(new Date(), 4);
   const endDate = addDays(new Date(), 7);
   const numOfGuests = 1;
-  const search = () => {
-    router.push({
-      pathname: "/search",
-      query: {
-        location: cityData.location,
-        id: cityData.id,
-        startDate: startDate.toISOString(),
-        endDate: endDate.toISOString(),
-        numOfGuests,
-      },
-    });
-  };
+  // const search = () => {
+  //   router.push({
+  //     pathname: "/search",
+  //     query: {
+  //       location: cityData.location,
+  //       id: cityData.id,
+  //       startDate: startDate.toISOString(),
+  //       endDate: endDate.toISOString(),
+  //       numOfGuests,
+  //     },
+  //   });
+  // };
+  const setSearchInputAndSelectedCity = () => {
+    setSearchInput(cityData.displayName);
+    setSelectedCity(cityData);
+  }
 
   return (
-    <div onClick={search} className="flex items-center m-2 space-x-4 rounded-xl cursor-pointer hover:bg-gray-100 hover:scale-105 transition transform duration-200 ease-out">
+    <div onClick={setSearchInputAndSelectedCity} className="flex items-center m-2 space-x-4 rounded-xl cursor-pointer hover:bg-gray-100 hover:scale-105 transition transform duration-200 ease-out">
       {/* Left side */}
       <div className="relative h-16 w-16">
         <Image
           className="rounded-lg"
-          src={cityData.img}
-          alt={cityData.location}
+          src={cityData.img!}
+          alt={cityData.location!}
           fill
         />
       </div>
       {/* Right side */}
       <div>
         <h2>{cityData.location}</h2>
-        <h3 className="text-gray-500">{cityData.province}</h3>
+        <h3 className="text-gray-500">{cityData.province!}</h3>
       </div>
     </div>
   );
